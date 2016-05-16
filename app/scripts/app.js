c4882346f40afdc68e49c5f0c1d6e4ded6eb4717
Leonardo Loyo <cyberedward@gmail.com>
2016-05-16 16:23:54 -0400

    Primer Push Cotizacion Express

---
+++ app/scripts/app.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+  var app = angular.module('cotizacionExpressApp', [
+      'ngAnimate',
+      'ngAria',
+      'ngCookies',
+      'ngMessages',
+      'ngResource',
+      'ngRoute',
+      'ngSanitize',
+      'ngTouch',
+      'ui.router'
+    ]);
+    app.config(function($stateProvider, $urlRouterProvider){
+      $stateProvider
+      .state('app',{
+        url:'/',
+        templateUrl:'index.html'
+      })
+      .state('login',{
+        url:'/login',
+        views:{
+          'maincontent':{
+            templateUrl:'/views/login.html',
+          }
+        }
+      })
+
+      $urlRouterProvider.otherwise('/');
+    });
+})();
---
+++ app/scripts/services/userservices.js
@@ -0,0 +1,17 @@
+(function(){
+  'use strict';
+
+  /**
+   * @ngdoc service
+   * @name cotizacionExpressApp.UserServices
+   * @description
+   * # UserServices
+   * Service in the cotizacionExpressApp.
+   */
+  var app = angular.module('cotizacionExpressApp');
+    app.service('UserServices', function () {
+      // AngularJS will instantiate a singleton by calling "new" on this function
+    });
+
+
+})();
