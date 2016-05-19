5daaf348a91a4ee8abb476d5f716f9e9ed38d188
jessicaariverol@gmail.com <jessicaariverol@gmail.com>
2016-05-19 09:51:48 -0430

    exportacion a excel

--- app/scripts/controllers/resumen.js
+++ app/scripts/controllers/resumen.js
@@ -1,6 +1,9 @@
 (function(){
   var app = angular.module('cotizacionExpressApp');
     app.controller('ResumenCtrl', function ($scope, Resumen,Cotizacion) {
-      
+        $scope.row = {
+            contenedores:"Canasto",
+            cantidadesCon:10,
+            metrosCon:15};
   });
 })();
