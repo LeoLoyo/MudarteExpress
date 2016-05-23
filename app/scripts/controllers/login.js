ad69b308b8831bbce82f50db98d795988c22a91d
Yohandri <yoha3001@gmail.com>
2016-05-23 10:08:26 -0430

    actualización 23-05-2016

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -2,14 +2,16 @@
   var app = angular.module('cotizacionExpressApp');
     app.controller('LoginCtrl', function ($scope, $state, Auth) {
       // console.log($state.data.current.session);
-      $scope.ingresar = function(user){
+      $('.btnsCotizacion').addClass('hidden');
+      $scope.ingresar = function(user,pass){
           if(Auth.valid(user)){
 
               $state.go('cotizacion');
+              $('.btnsCotizacion').removeClass('hidden');
           }else{
               console.log("usuario invalido");
+              $('.spanErrorUser').removeClass('hidden');
           }
-
       }
 
     angular.element('#cUsuario').focus();
