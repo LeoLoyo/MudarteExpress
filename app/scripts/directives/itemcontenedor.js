0dbe0b2aa1c403b4e5130e0b0d32521f064b08a9
jessicaariverol@gmail.com <jessicaariverol@gmail.com>
2016-06-08 13:46:26 -0430

    CambiosResumenyKM

--- app/scripts/directives/itemcontenedor.js
+++ app/scripts/directives/itemcontenedor.js
@@ -9,13 +9,7 @@
 var exportTable = function(){
 var link = function($scope, elm, attr){
 $scope.$on("export-pdf", function(e, d){
-      elm.tableExport({type:"pdf", escape:"false"});
- });
-$scope.$on("export-excel", function(e, d){
-       elm.tableExport({type:"excel", escape:false});
- });
-$scope.$on("export-doc", function(e, d){
-     elm.tableExport({type: "doc", escape:false});
+      $('.export-table').tableExport({type:'pdf',escape:'false'});
  });
 }
 return {
